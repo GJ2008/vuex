e2f1ffd109e6a824ef313eae5c58657bba6d8d3d
dage <599161490@qq.com>
2021-07-16 15:26:56 +0800

    简单vuex

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <p>{{cartName}}</p>
+  <p>{{bbbName}}</p>
+  <p>{{productName}}</p>
+
+  <button @click="saveCartAction('aaa')">cart</button>
+  <button @click="saveBbbAction('b2b2bb2')">product</button>
+  <button @click="saveProductAction('p2p2p2p')">bbb</button>
+  <button @click="saveCartMutation('aaa')">cart</button>
+  <button @click="saveBbbMutation('b2b2bb2')">product</button>
+  <button @click="saveProductMutation('p2p2p2p')">bbb</button>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import {mapActions, mapState, mapMutations} from 'vuex'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  computed: {
+    ...mapState({
+      cartName: (state)=>state.cart.name,
+      productName: (state)=>state.products.name,
+      bbbName: (state)=>state.cart.bbb.name
+    })
+  },
+  methods: {
+    ...mapActions({
+      saveCartAction: 'cart/saveName',
+      saveBbbAction: 'cart/bbb/saveName',
+      saveProductAction: 'products/saveName'
+    }),
+    ...mapMutations({
+      saveCartMutation: 'cart/SAVE_NAME',
+      saveBbbMutation: 'cart/bbb/SAVE_NAME',
+      saveProductMutation: 'products/SAVE_NAME'
+    })
   }
 }
 </script>
---
+++ src/store/modules/cart/bbb.js
@@ -0,0 +1,27 @@
+  const state=() =>({
+    name: 'bbb'
+  })
+  const mutations= {
+    SAVE_NAME(state, name){
+        state.name = name
+    }
+  }
+  const actions= {
+    saveName ({ commit }, name) {
+        let name1 = name + '但是我中转了一下'
+        commit('SAVE_NAME',name1)
+    }
+  }
+  const getters= {
+    saveName ({ commit }, name) {
+      commit('SAVE_NAME',name)
+    }
+  }
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+  }
